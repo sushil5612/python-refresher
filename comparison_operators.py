b4366f030405b2ea22e2b2ad611202a9cf9d499d
Sushil Behera <sushil.behera@gmail.com>
2026-09-16 21:32:34 -0500

    initial commit

---
+++ comparison_operators.py
@@ -0,0 +1,10 @@
+# Taking two numbers as input from the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+# Applying comparison operators and displaying results
+print(f"{a} > {b} :", a > b)
+print(f"{a} < {b} :", a < b)
+print(f"{a} >= {b} :", a >= b)
+print(f"{a} <= {b} :", a <= b)
+print(f"{a} == {b} :", a == b)
+print(f"{a} != {b} :", a != b)
